0e14ebce577f9fca963a94d52b79b99a74031c1b
SirBrusEd2 <andreyvasiliev8952@mail.ru>
2025-05-22 22:46:48 +0300

    Wiki description

--- src/main/java/com/example/lab2dao/dao/PostgresProductDaoImpl.java
+++ src/main/java/com/example/lab2dao/dao/PostgresProductDaoImpl.java
@@ -50,6 +50,14 @@ public class PostgresProductDaoImpl implements ProductDao {
     /**
      * Создает таблицу products, если она не существует.
      * @throws SQLException при ошибках выполнения DDL-запроса
+     *
+     * <p>Структура таблицы:
+     * <ul>
+     *     <li>id - SERIAL (автоинкремент)</li>
+     *     <li>name - VARCHAR(255) NOT NULL</li>
+     *     <li>quantity - INTEGER NOT NULL</li>
+     *     <li>tag - VARCHAR(255)</li>
+     * </ul>
      */
     private void createTableIfNotExists() throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS products (" +
@@ -66,6 +74,13 @@ public class PostgresProductDaoImpl implements ProductDao {
     /**
      * Добавляет новый товар в базу данных.
      * @param product добавляемый товар (ID генерируется базой данных)
+     *
+     * <p>Логика работы:
+     * <ol>
+     *     <li>Выполняет INSERT без указания ID</li>
+     *     <li>Получает сгенерированный ID через RETURN_GENERATED_KEYS</li>
+     *     <li>Обновляет ID в переданном объекте Product</li>
+     * </ol>
      */
     @Override
     public void addProduct(Product product) {
@@ -82,6 +97,13 @@ public class PostgresProductDaoImpl implements ProductDao {
     /**
      * Обновляет данные существующего товара в базе.
      * @param product товар с обновленными данными (ищется по ID)
+     *
+     * <p>Особенности:
+     * <ul>
+     *     <li>Обновляет все поля кроме ID</li>
+     *     <li>Использует PreparedStatement для защиты от SQL-инъекций</li>
+     *     <li>Не проверяет существование товара перед обновлением</li>
+     * </ul>
      */
     @Override
     public void updateProduct(Product product) {
@@ -98,6 +120,13 @@ public class PostgresProductDaoImpl implements ProductDao {
     /**
      * Удаляет товар по указанному ID из базы данных.
      * @param id идентификатор удаляемого товара
+     *
+     * <p>Примечания:
+     * <ul>
+     *     <li>Не проверяет существование записи перед удалением</li>
+     *     <li>Возвращает успешный статус даже если товар не найден</li>
+     *     <li>Не удаляет связанные записи в других таблицах</li>
+     * </ul>
      */
     @Override
     public void deleteProduct(int id) {
@@ -113,6 +142,13 @@ public class PostgresProductDaoImpl implements ProductDao {
     /**
      * Получает список всех товаров из базы данных.
      * @return список товаров. Возвращает пустой список при ошибках выполнения запроса.
+     *
+     * <p>Особенности:
+     * <ul>
+     *     <li>Использует простой Statement (нет параметризованных значений)</li>
+     *     <li>Не поддерживает пагинацию</li>
+     *     <li>Может вызвать OutOfMemoryError при большом количестве записей</li>
+     * </ul>
      */
     @Override
     public List<Product> getAllProducts() {
@@ -129,11 +165,35 @@ public class PostgresProductDaoImpl implements ProductDao {
         return products;
     }
 
+    /**
+     * Устанавливает источник данных для реализации DAO.
+     * @param source специфичный для реализации идентификатор источника данных
+     *
+     * <p>В текущей реализации:
+     * <ul>
+     *     <li>Не поддерживается (заглушка)</li>
+     *     <li>Может быть реализовано переключение на другую БД</li>
+     * </ul>
+     */
     @Override
     public void setDataSource(String source) {
         // Реализация переключения БД может быть добавлена здесь
     }
 
+    /**
+     * Создает объект Product из ResultSet.
+     * @param rs ResultSet с текущей позицией курсора
+     * @return новый объект Product
+     * @throws SQLException при ошибках чтения данных
+     *
+     * <p>Ожидаемая структура ResultSet:
+     * <ul>
+     *     <li>id - INTEGER</li>
+     *     <li>name - VARCHAR</li>
+     *     <li>quantity - INTEGER</li>
+     *     <li>tag - VARCHAR</li>
+     * </ul>
+     */
     private Product extractProductFromResultSet(ResultSet rs) throws SQLException {
         return new Product(
                 rs.getInt("id"),
@@ -143,12 +203,38 @@ public class PostgresProductDaoImpl implements ProductDao {
         );
     }
 
+    /**
+     * Устанавливает параметры продукта в PreparedStatement.
+     * @param pstmt подготовленный запрос
+     * @param product объект продукта
+     * @throws SQLException при ошибках установки параметров
+     *
+     * <p>Устанавливает параметры в следующем порядке:
+     * <ol>
+     *     <li>name (String)</li>
+     *     <li>quantity (int)</li>
+     *     <li>tag (String)</li>
+     * </ol>
+     */
     private void setProductParameters(PreparedStatement pstmt, Product product) throws SQLException {
         pstmt.setString(1, product.getName());
         pstmt.setInt(2, product.getQuantity());
         pstmt.setString(3, product.getTag());
     }
 
+    /**
+     * Обновляет ID продукта из сгенерированных ключей.
+     * @param pstmt PreparedStatement с сгенерированными ключами
+     * @param product объект для обновления
+     * @throws SQLException при ошибках чтения ключей
+     *
+     * <p>Логика работы:
+     * <ul>
+     *     <li>Читает первый сгенерированный ключ</li>
+     *     <li>Устанавливает его как ID продукта</li>
+     *     <li>Ничего не делает если ключи отсутствуют</li>
+     * </ul>
+     */
     private void updateProductIdFromGeneratedKeys(PreparedStatement pstmt, Product product) throws SQLException {
         try (ResultSet rs = pstmt.getGeneratedKeys()) {
             if (rs.next()) {
@@ -157,6 +243,17 @@ public class PostgresProductDaoImpl implements ProductDao {
         }
     }
 
+    /**
+     * Отображает ошибку подключения через JavaFX Alert.
+     * @param e исключение с деталями ошибки
+     *
+     * <p>Отображает:
+     * <ul>
+     *     <li>Заголовок: "Ошибка подключения"</li>
+     *     <li>Сообщение: текст исключения</li>
+     *     <li>Тип: ERROR</li>
+     * </ul>
+     */
     private void showConnectionErrorAlert(SQLException e) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Ошибка подключения");
@@ -165,8 +262,18 @@ public class PostgresProductDaoImpl implements ProductDao {
         alert.showAndWait();
     }
 
+    /**
+     * Обрабатывает SQL-исключения.
+     * @param e исключение для обработки
+     *
+     * <p>Текущая реализация:
+     * <ul>
+     *     <li>Выводит stacktrace в консоль</li>
+     *     <li>Не прерывает выполнение программы</li>
+     *     <li>Может быть расширена для логирования в файл</li>
+     * </ul>
+     */
     private void handleSQLException(SQLException e) {
         e.printStackTrace();
-        // Здесь может быть добавлена дополнительная обработка ошибок
     }
 }
